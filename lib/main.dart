fd2476638c8a3e1ed4bdcd2ec4c93df7ec136318
Michael Dadzie <dadziemikke2@gmail.com>
2021-01-29 02:44:37 +0000

    Initialize Camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(Dentify());
 }
 
@@ -20,7 +25,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.red,
       ),
     );
   }
