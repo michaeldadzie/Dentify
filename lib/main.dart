2ebd4bfd6423a2f0476fda8525c2d1a70099df89
Michael Dadzie <dadziemikke2@gmail.com>
2021-02-15 23:55:00 +0000

    Code Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:camera/camera.dart';
-import 'screens/tab_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'screens/tab_screen.dart';
 
 List<CameraDescription> cameras;
 
